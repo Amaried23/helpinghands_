4053df1b3c9616cf6eb452055a0977e7051094b8
ALEXIS DELUCIA <alexisdelucia23@gmail.com>
2018-05-31 17:39:09 -0400

    fixed config file and models file

--- app/models/index.js
+++ app/models/index.js
@@ -10,21 +10,15 @@ var config;
 
 // require(__dirname + '/../config/config.json')[env];
 
-if (process.platform !== "win32") {
-  config = require(__dirname + '/../config/config.json')[env];
-} else {
-  config = require(__dirname + '/..\\config\\config.json')[env];
-}
-
-
-var db        = {};
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
 fs
   .readdirSync(__dirname)
   .filter(file => {
